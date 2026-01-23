6bb9736c4407911038798f585826b381e352741a
hal3e <git@hal3e.io>
2026-01-23 12:20:50 +0100

    chore: remove `Evtr` struct

--- src/device.rs
+++ src/device.rs
@@ -1,43 +1,5 @@
 mod monitor;
 mod selector;
 
-use crate::error::Result;
 pub use monitor::DeviceMonitor;
-use ratatui::DefaultTerminal;
 pub use selector::{DeviceInfo, DeviceSelector};
-
-pub struct Evtr {
-    terminal: DefaultTerminal,
-}
-
-impl Evtr {
-    pub fn new() -> Result<Self> {
-        let terminal = ratatui::init();
-
-        Ok(Self { terminal })
-    }
-
-    pub async fn run(mut self) -> Result<()> {
-        let result = self.run_loop().await;
-        ratatui::restore();
-
-        result
-    }
-
-    async fn run_loop(&mut self) -> Result<()> {
-        let mut error_message: Option<String> = None;
-        loop {
-            let Some(device) =
-                DeviceSelector::run(&mut self.terminal, error_message.take()).await?
-            else {
-                break;
-            };
-
-            if let Err(err) = DeviceMonitor::run(&mut self.terminal, device).await {
-                error_message = Some(err.to_string());
-            }
-        }
-
-        Ok(())
-    }
-}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 mod device;
 mod error;
 
-use crate::error::Result;
+use crate::device::{DeviceMonitor, DeviceSelector};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    device::Evtr::new()?.run().await
+async fn main() -> error::Result<()> {
+    let mut terminal = ratatui::init();
+
+    let mut error_msg = None;
+    loop {
+        let Some(device) = DeviceSelector::run(&mut terminal, error_msg.take()).await? else {
+            break;
+        };
+
+        if let Err(err) = DeviceMonitor::run(&mut terminal, device).await {
+            error_msg = Some(err.to_string());
+        }
+    }
+
+    ratatui::restore();
+
+    Ok(())
 }
